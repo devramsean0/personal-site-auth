7d9007679073e0422da13485bcc9cb5f45295cb6
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-05-16 12:43:24 +0100

    discord Api creation

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(50451, () => {
+  console.info('Running on port 50451');
+});
+// Routes
+app.use('/api/discord', require('./api/discord'));
