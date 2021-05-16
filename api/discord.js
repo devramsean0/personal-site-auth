61760733fc4e358a71132d6f5dbc0586a05837a4
Outramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-05-16 16:31:30 +0100

    more api stuff

--- api/discord.js
+++ api/discord.js
@@ -1,13 +1,11 @@
 const express = require('express');
-
-const router = express.Router();
-
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const router = express.Router();
 const redirect = encodeURIComponent('http://localhost:50451/api/discord/callback');
 
-router.get('/login', (req, res) => {
-  res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${redirect}`);
+router.get('/LOGIN', (req, res) => {
+  res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${CLIENT_ID}scope=identify&response_type=code&redirect_uri=${redirect}`);
 });
 
 module.exports = router;
